9a098a35e09e5966dea3478bc0379a296b9f647b
GiCheols <ngc4665@naver.com>
2023-02-08 22:45:33 +0900

    1003 피보나치 함수

---
+++ Silver/1003.py
@@ -0,0 +1,14 @@
+fibo_one = []
+fibo_zero = []
+fibo_zero.append(1)
+fibo_zero.append(0)
+fibo_one.append(0)
+fibo_one.append(1)
+
+for i in range(2, 41):
+    fibo_zero.append(fibo_zero[i-1] + fibo_zero[i-2])
+    fibo_one.append(fibo_one[i-1] + fibo_one[i-2])
+
+for _ in range(int(input())):
+    n = int(input())
+    print(fibo_zero[n], fibo_one[n])
